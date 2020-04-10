f1ff3be84ebd9792c382a3d5939b1323149b35c4
Mauro Bianchi <bianchimro@gmail.com>
2020-04-10 14:01:56 +0200

    Updates

---
+++ precache-manifest.ad156a8e16dc6bc4bbe51d90be6369f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a99785d79330fcd83542b27bc72033c",
+    "url": "/raw-mapping-playground/index.html"
+  },
+  {
+    "revision": "e71f1e252f2307e482ff",
+    "url": "/raw-mapping-playground/static/css/2.aa9cacd8.chunk.css"
+  },
+  {
+    "revision": "5789216d017df4941afe",
+    "url": "/raw-mapping-playground/static/css/main.27b55afe.chunk.css"
+  },
+  {
+    "revision": "e71f1e252f2307e482ff",
+    "url": "/raw-mapping-playground/static/js/2.c5408461.chunk.js"
+  },
+  {
+    "revision": "93ccbcd426ada3d22809924b1fd3cc69",
+    "url": "/raw-mapping-playground/static/js/2.c5408461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5789216d017df4941afe",
+    "url": "/raw-mapping-playground/static/js/main.7d932b03.chunk.js"
+  },
+  {
+    "revision": "bf4f34594c4a48b610f0",
+    "url": "/raw-mapping-playground/static/js/runtime-main.4de6b8ed.js"
+  }
+]);
